498f32ff859e915695770c9c1af142cd47f511ca
Shinyvv <danielignacio252@gmail.com>
2023-11-04 18:31:12 -0300

    Eliminacion HTML y finalizacion HBS

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const express = require ('express');
 const app = express();
-
 const port = 4000;
 
-app.use(express.static(__dirname + "/public"));
-
-app.use((req, res, next)=> {res.status(404).sendfile(__dirname +"/public/404.html")})
-
-app.listen(port, ()=> {console.log('Sv al servicio', port)})
-
 // Motor de plantilla
+
 const hbs = require('hbs');
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 app.set('view engine', 'hbs');
@@ -17,10 +11,70 @@ app.set("views", __dirname + "/views");
 
 app.use(express.static(__dirname + "/public"));
 
+
 //rutas
 
-app.get('/', (req, res)=>{res.render('index', {titulo: 'Inicio'})})
+app.get('/', (req, res) => {
+  res.render('inicio', {
+  titulo: 'Inicio'});
+});
 
+app.get('/NewVotacion', (req, res) => {
+  res.render('NewVotacion', {
+  titulo:'Crear Votacion',
+  descripcion: 'Crea una votacion nueva',
+  estado: true });
+});
+app.get('/ingreso', (req, res) => {
+  res.render('ingreso', {
+  titulo:'Ingresar',
+  descripcion: 'Ingresa como usuario',
+  estado: true });
+});
+app.get('/RegistroListo', (req, res) => {
+  res.render('RegistroListo', {
+  titulo:'Registro Completado',
+  descripcion: 'Gracias por registrarse',
+  estado: true });
+});
+app.get('/registro', (req, res) => {
+  res.render('registro', {
+  titulo:'Registro',
+  descripcion: 'Apartado de registro de usuarios',
+  estado: true });
+});
+app.get('/Resultado 1', (req, res) => {
+  res.render('Resultado 1', {
+  titulo:'Resultado',
+  descripcion: 'Aqui se muestran los resultados',
+  estado: true });
+});
+app.get('/Resultado 2', (req, res) => {
+  res.render('Resultado 2', {
+  titulo:'Resultado',
+  descripcion: 'Aqui se muestran los resultados',
+  estado: true });
+});
+app.get('/Resultado 3', (req, res) => {
+  res.render('Resultado 3', {
+  titulo:'Resultado',
+  descripcion: 'Aqui se muestran los resultados',
+  estado: true });
+});
+app.get('/Resultado 4', (req, res) => {
+  res.render('Resultado 4', {
+  titulo:'Resultado',
+  descripcion: 'Aqui se muestran los resultados',
+  estado: true });
+});
+app.get('/Votaciones', (req, res) => {
+  res.render('Votaciones', {
+  titulo:'Votaciones',
+  descripcion: 'Estado de las votaciones',
+  estado: true });
+});
+
+app.listen(port, ()=> {console.log('Sv al servicio', port)})
 //conexion a DB
 
 const mongoose = require('mongoose');
